6e1938bae8d9f76b1c01d9cdcfcb12a57f054c42
simonproudfoot <simonproudfoot1980@gmail.com>
2021-08-18 15:06:20 +0100

    animation poses

--- src/components/combo.vue
+++ src/components/combo.vue
@@ -2,15 +2,19 @@
 <div class="" style="position: relative">
     <div id="container"></div>
     <div class="controls">
-        <button @click="rotate('x', 1, 1)">Wobble X</button>
-        <button @click="rotate('y', 2, 1)">Wobble Y </button>
-        <button @click="rotate('z', 2, 1)">Wobble Z</button>
+        <button @click="defaultPosition">RESET</button>
+        <button @click="rotate('x', 0.5, 1, true)">Wobble X</button>
+        <button @click="rotate('y', 0.5, 1, true)">Wobble Y </button>
+        <button @click="rotate('z', 0.5, 1, true)">Wobble Z</button>
+        <button @click="move('y', 1, 1, true)">Move up and down</button>
+        <button @click="move('x', 1, 1, true)">Move left and right</button>
         <button :class="sideFin === 'side_fin_1' ? 'active' : null" @click="changeSideFin('side_fin_1')">Side fin 1</button>
         <button :class="sideFin === 'side_fin_2' ? 'active' : null" @click="changeSideFin('side_fin_2')">Side fin 2</button>
         <span style="padding: 0 10px">|</span>
         <button :class="backFin === 'back_fin_1' ? 'active' : null" @click="changeBackFin('back_fin_1')">Back fin 1</button>
         <button :class="backFin === 'back_fin_2' ? 'active' : null" @click="changeBackFin('back_fin_2')">Back fin 2</button>
-        <vue-easy-lightbox :visible="visible" :imgs="imageTwo" @hide="visible = false"></vue-easy-lightbox>
+        <span style="padding: 0 10px">|</span>
+        <button @click="sink">Sink</button>
     </div>
 </div>
 </template>
@@ -18,17 +22,20 @@
 <script>
 import * as Three from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import VueEasyLightbox from 'vue-easy-lightbox'
 import { gsap, Linear } from "gsap";
 export default {
     name: 'ThreeTest',
-    components: { VueEasyLightbox },
     data() {
         return {
-            start: {
+            startAxis: {
                 x: 0.2,
                 y: 10,
-                z: 0
+                z: 0,
+            },
+            startPosition: {
+                x: 0,
+                y: 1,
+                z: 0,
             },
             visible: false,
             imageTwo: require('@/assets/fish-guide2.jpg'),
@@ -45,64 +52,93 @@ export default {
         }
     },
     methods: {
-        rotate(axis, angle, speed) {
-            angle = this.convertDecimal(angle)
-            gsap.killTweensOf(this.fishOne.rotation, axis);
-            gsap.set(this.fishOne.rotation, {
-                [axis]: this.start[axis]
-            })
-            gsap.fromTo(this.fishOne.rotation, speed, {
-                [axis]: this.fishOne.rotation[axis] + angle.start
-            }, {
-                [axis]: this.fishOne.rotation[axis] + angle.end,
-                yoyo: true,
-                repeat: -1,
-                ease: "sine.inOut",
-                autoRound: false
-            });
+
+        // Swim animations
+        // axis, angle, speed, loop
+
+        sink() {
+            this.changeSpeed(10)
+            this.rotate('x', 1, 3, false)
+            this.rotate('z', 1, 1, true)
+            this.move('y', -20, 5, false)
+            setTimeout(() => {
+                this.defaultPosition()
+            }, 4000);
         },
-       
-        convertDecimal(num) {
+
+        // movement functions
+        changeSpeed(s) {
+            this.mixer.timeScale = s
+        },
+
+        rotate(axis, angle, speed, loop) {
+            angle = this.convertDecimal(angle, true)
+            console.log(JSON.stringify(angle))
+          //  gsap.killTweensOf(this.fishOne.rotation, axis);
+            // gsap.set(this.fishOne.rotation, {
+            //     [axis]: this.startAxis[axis]
+            // })
+            if (loop) {
+                gsap.fromTo(this.fishOne.rotation, speed, {
+                    [axis]: this.fishOne.rotation[axis] + angle.start
+                }, {
+                    [axis]: this.fishOne.rotation[axis] + angle.end,
+                    yoyo: true,
+                    repeat: -1,
+                    ease: "sine.inOut",
+                    autoRound: false
+                });
+            } else {
+                gsap.to(this.fishOne.rotation, speed, {
+                    [axis]: this.fishOne.rotation[axis] + angle.end,
+                    ease: "sine.inOut",
+                });
+            }
+
+        },
+        move(direction, distance, speed, loop) {
+            distance = this.convertDecimal(distance, true)
+            console.log(JSON.stringify(distance))
+          //  gsap.killTweensOf(this.fishOne.position, direction);
+            // gsap.set(this.fishOne.position, {
+            //     [direction]: this.startPosition[direction]
+            // })
+            if (loop) {
+                gsap.fromTo(this.fishOne.position, speed, {
+                    [direction]: this.fishOne.position[direction] + distance.start
+                }, {
+                    [direction]: this.fishOne.position[direction] + distance.end,
+                    yoyo: true,
+                    repeat: -1,
+                    ease: "sine.inOut",
+                    autoRound: false
+                });
+            } else {
+                gsap.to(this.fishOne.position, speed, {
+                    [direction]: this.fishOne.rotation[direction] + distance.end,
+                    ease: "sine.inOut",
+                });
+            }
+        },
+        convertDecimal(num, removeDecimal) {
             let decimal = '0.' + num
             let converted = parseFloat(decimal)
             let x = converted + converted
-            return { start: x - decimal - decimal - decimal, end: converted }
+            return !removeDecimal ? { start: x - decimal - decimal - decimal, end: converted } : { start: num - num - num, end: num }
         },
-        reset() {
-            this.moveY(this.start.y)
-            this.moveX(0)
-        },
-
-        moveY(start, end) {
-            gsap.set(this.fishOne.position, { y: 0 })
-            if (end) {
-                gsap.fromTo(this.fishOne.position, 1, { z: this.fishOne.position.y + start }, {
-                    y: this.fishOne.position.y + end,
-                    yoyo: true,
-                    repeat: -1,
-                    ease: "sine.inOut",
-                    autoRound: false
-                });
-            } else {
-                gsap.to(this.fishOne.rotation, 5, { z: 50, repeat: -1, ease: false });
-            }
-        },
-        moveX(start, end) {
-            //   gsap.set(this.fishOne.position, { x: this.start.z })
-            if (end) {
-                gsap.fromTo(this.fishOne.position, 1, { z: this.fishOne.position.x + start }, {
-                    x: this.fishOne.position.x + end,
-                    yoyo: true,
-                    repeat: -1,
-                    ease: "sine.inOut",
-                    autoRound: false
-                });
-            } else {
-                gsap.to(this.fishOne.rotation, 5, { z: 50, repeat: -1, ease: false });
-            }
-        },
-        changeSpeed(s) {
-            this.mixer.timeScale = s
+        defaultPosition() {
+            this.changeSpeed(1)
+            gsap.killTweensOf(this.fishOne.position, 'x,y,z');
+            gsap.killTweensOf(this.fishOne.rotation, 'x,y,z');
+            this.fishOne.rotation.y = this.startAxis.y
+            this.fishOne.rotation.x = this.startAxis.x
+            this.fishOne.rotation.z = this.startAxis.z
+            this.fishOne.position.y = this.startPosition.y
+            this.fishOne.position.x = this.startPosition.x
+            this.fishOne.position.z = this.startPosition.z
+            this.rotate('x', 0.1, 2, true)
+            this.rotate('z', 0.1, 2, true)
+            this.move('y', 0.1, 3, true)
         },
         onWindowResize() {
             this.camera.aspect = window.innerWidth / window.innerHeight;
@@ -116,13 +152,13 @@ export default {
                 //   this.moveY(-0.5, 0.5, 1)
                 // this.moveX(0, 0, 0)
                 //  this.rotateY(0, 0, 0)
-                this.rotateX(-0.1, 0.1, 0.6)
+                //   this.rotateX(-0.1, 0.1, 0.6)
                 this.fishOne.getObjectByName('back_fin_2').visible = true
                 this.fishOne.getObjectByName('back_fin_1').visible = false
             }
             if (val == 'back_fin_1') {
                 //     this.rotateX(0, 0, 0)
-                this.rotateY(-0.1, 0.1, 0.6)
+                //   this.rotateY(-0.1, 0.1, 0.6)
                 //   this.moveY(0, 0, 0)
                 //    this.moveX(-0.5, 0.5, 1)
                 this.fishOne.getObjectByName('back_fin_1').visible = true
@@ -133,12 +169,12 @@ export default {
         changeSideFin(val) {
             this.sideFin = val
             if (val == 'side_fin_2') {
-                this.moveY(-0.5, 0.5, 1)
+                // this.moveY(-0.5, 0.5, 1)
                 this.fishOne.getObjectByName('side_fin_2').visible = true
                 this.fishOne.getObjectByName('side_fin_1').visible = false
             }
             if (val == 'side_fin_1') {
-                this.moveY(-0.1, 0.1, 0.5)
+                // this.moveY(-0.1, 0.1, 0.5)
                 this.fishOne.getObjectByName('side_fin_1').visible = true
                 this.fishOne.getObjectByName('side_fin_2').visible = false
                 //this.fishOne.rotation.y = 0
@@ -176,15 +212,15 @@ export default {
 
                 this.fishOne = gltf.scene
                 this.scene.add(this.fishOne);
-                this.fishOne.rotation.y = this.start.y
-                this.fishOne.rotation.x = this.start.x
-                this.fishOne.rotation.z = this.start.z
+                this.fishOne.rotation.y = this.startAxis.y
+                this.fishOne.rotation.x = this.startAxis.x
+                this.fishOne.rotation.z = this.startAxis.z
                 this.fishOne.getObjectByName('body').rotation.y = 0
                 this.fishOne.getObjectByName(this.backFin).visible = true
                 this.fishOne.getObjectByName('back_fin_2').visible = false
                 this.fishOne.getObjectByName(this.sideFin).visible = true
                 this.fishOne.getObjectByName('side_fin_2').visible = false
-                this.moveY(-0.1, 0.1, 2)
+                this.defaultPosition()
 
             })
             // RENDER
